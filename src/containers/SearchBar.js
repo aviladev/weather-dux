e03d64125a812e0491442da5984ce89ad847ec95
Pedro Ávila <pedroavila95@gmail.com>
2018-01-20 01:37:01 -0200

    Make SearchBar's input a controlled field and add styles

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 
 class SearchBar extends Component {
+  state = { term: '' }
+
+  onInputChange = ({
+    target: {value: term}
+  }) => this.setState({ term })
+
   render () {
     return (
         <form className="input-group">
-          <input />
+          <input
+            className="form-control"
+            placeholder="Get a five day forecast in your favorite cities"
+            value={this.state.term}
+            onChange={this.onInputChange}
+          />
           <span className="input-group-btn">
             <button type="submit" className="btn btn-secondary">
               Submit
